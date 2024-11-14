4023af1906198add3e6368266abe79cea4ef9caa
josh <jianlimpiado@gmail.com>
2024-11-15 00:31:06 +0800

    feat: initial animation timer

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -1,8 +1,27 @@
 import styles from './timer.module.css';
+import {clsx} from 'clsx';
+import {useTimerContext} from "@/context/timerContext.tsx";
+import {useEffect, useState} from "react";
 
 const Timer = () => {
+    const {state} = useTimerContext();
+    const [isRunning, setIsRunning] = useState(false);
+
+    useEffect(() => {
+        switch(state) {
+            case "STOP":
+            case "PAUSE":
+                setIsRunning(false);
+                break;
+            case "START":
+                setIsRunning(true);
+        }
+    }, [state])
+
     return (
-        <div className={styles.timer_container}>
+        <div className={clsx(styles.timer_container, {
+            [styles.active_timer]: isRunning
+        })}>
             <p>25<br />00</p>
         </div>
     )
