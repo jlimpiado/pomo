0e0a980f0133b30af212c9b1d303d67e7b78dcac
josh <jianlimpiado@gmail.com>
2024-11-19 23:35:00 +0800

    feat: goto next state countdown done

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -3,9 +3,11 @@ import {clsx} from 'clsx';
 import {useTimerContext} from "@/context/timerContext.tsx";
 import {useEffect, useState} from "react";
 import {formatTime} from "@/helpers.ts";
+import {useUserContext} from "@/context/userProvider.tsx";
 
 const Timer = () => {
     const {state, currentTime} = useTimerContext();
+    const {gotoNextState} = useUserContext();
     const [isRunning, setIsRunning] = useState(false);
     const [minutes, setMinutes] = useState(() => Math.floor(currentTime / 60));
     const [seconds, setSeconds] = useState(() => currentTime % 60);
@@ -29,7 +31,11 @@ const Timer = () => {
     useEffect(() => {
         setMinutes(Math.floor(currentTime / 60));
         setSeconds(currentTime % 60);
-    }, [currentTime]);
+
+        if(currentTime === 0) {
+            gotoNextState();
+        }
+    }, [currentTime, gotoNextState]);
 
     return (
         <div className={clsx(styles.timer_container, {
