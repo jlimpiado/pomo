a709471ef0be4d4d2d32be698c6432f24bcbbd1d
josh <jianlimpiado@gmail.com>
2024-11-28 00:13:01 +0800

    feat: update number input component

--- src/components/ui/numberInput/index.tsx
+++ src/components/ui/numberInput/index.tsx
@@ -1,15 +1,43 @@
 import styles from './numberInput.module.css';
 import ArrowIcon from '@/assets/icons/arrow.svg';
+import {ChangeEvent, FC, useEffect, useState} from "react";
+import {NumberInputProps} from "@/types.ts";
 
-const NumberInput = () => {
+const NumberInput: FC<NumberInputProps> = (props) => {
+    const {
+        defaultValue,
+        onChange
+    } = props;
+    const [count, setCount] = useState(() => defaultValue);
+
+    const incrementCount = () => {
+        setCount(prev => prev + 1);
+    }
+
+    const decrementCount = () => {
+        setCount(prev => {
+            const nextCount = prev - 1;
+            if(nextCount <= 0) return 0;
+            return nextCount;
+        });
+    }
+
+    const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setCount(Number(event.target.value))
+    }
+
+    useEffect(() => {
+        onChange(count)
+    }, [count, onChange]);
+
     return (
         <div className={styles.container}>
-            <input className={styles.input} type="number"/>
+            <input className={styles.input} type="number" value={count} onChange={handleOnChange}/>
             <section>
-                <button>
+                <button className={styles.btn_up} onClick={incrementCount}>
                     <ArrowIcon />
                 </button>
-                <button>
+                <button className={styles.btn_down} onClick={decrementCount}>
                     <ArrowIcon />
                 </button>
             </section>
